0417121a0f22bad6dd4c64d5e1f0c2ab91c5f86b
Dmitry <konstantinov.dmitry@gmail.com>
2010-01-26 16:12:58 +0300

    fixing chrome enter code && refactoring

--- jquery.quickSubmit.js
+++ jquery.quickSubmit.js
@@ -2,7 +2,7 @@ $.fn.quickSubmit = function() {
 	return this.each(function(){
 		$.fn.quickSubmit.form = $(this);
 		$(':input', $(this)).live('keypress', function(e){
-			if (e.which == 13 && $.fn.quickSubmit.isCtrl) {
+			if ($.fn.quickSubmit.enterCheck[e.which] && $.fn.quickSubmit.isCtrl) {
 				if ($('input[type="submit"]', $.fn.quickSubmit.form).length == 0)
 					$('input[type="button"]:last', $.fn.quickSubmit.form).click();
 				else
@@ -11,9 +11,20 @@ $.fn.quickSubmit = function() {
 		});
 	});
 }
+$.fn.quickSubmit.enterCheck = {
+	"10":1,
+	"13":1
+};
+$.fn.quickSubmit.ctrlCheck = {
+	"17":1,
+	"91":1,
+	"93":1,
+	"224":1
+};
 $(document).keyup(function (e) {
-	if((e.which == 17) || (e.which == 91) || (e.which == 93) || (e.which == 224)) 
+	if ($.fn.quickSubmit.ctrlCheck[e.which])
 		$.fn.quickSubmit.isCtrl=false;
 }).keydown(function (e) {
-	if((e.which == 17) || (e.which == 91) || (e.which == 93) || (e.which == 224)) $.fn.quickSubmit.isCtrl=true;
+	if ($.fn.quickSubmit.ctrlCheck[e.which])
+		$.fn.quickSubmit.isCtrl=true;
 });
